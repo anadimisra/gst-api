fb51306bf0d2b1d842fbec1b0a3cc49a86d075f6
Anadi Misra <anadi.misra@agilityroots.com>
2019-02-02 20:34:54 +0530

    enabled jpa caching

--- src/main/java/com/agilityroots/invoicely/DataApiJpaConfiguration.java
+++ src/main/java/com/agilityroots/invoicely/DataApiJpaConfiguration.java
@@ -4,6 +4,7 @@
 package com.agilityroots.invoicely;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = { "com.agilityroots.invoicely.repository" })
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EnableCaching
 public class DataApiJpaConfiguration {
 
 }
