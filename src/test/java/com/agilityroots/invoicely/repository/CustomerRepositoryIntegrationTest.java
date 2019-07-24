f26a0c2f22a068955662e46cf060f141a0287415
Anadi Misra <anadi.misra@agilityroots.com>
2019-07-24 10:32:13 +0530

    cleaned up unnecessary tests

--- src/test/java/com/agilityroots/invoicely/repository/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/agilityroots/invoicely/repository/CustomerRepositoryIntegrationTest.java
@@ -7,26 +7,18 @@ package com.agilityroots.invoicely.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
-
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.agilityroots.invoicely.DataApiJpaConfiguration;
-import com.agilityroots.invoicely.EntityObjectsBuilder;
-import com.agilityroots.invoicely.entity.Branch;
-import com.agilityroots.invoicely.entity.Contact;
 import com.agilityroots.invoicely.entity.Customer;
 
 import lombok.extern.slf4j.Slf4j;
@@ -39,66 +31,24 @@ import lombok.extern.slf4j.Slf4j;
 @RunWith(SpringRunner.class)
 @DataJpaTest(showSql = false)
 @ContextConfiguration(classes = { DataApiJpaConfiguration.class })
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource(locations = {"classpath:application-it.properties","classpath:application-test.properties"})
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class CustomerRepositoryIntegrationTest {
 
   @Autowired
   private CustomerRepository customerRepository;
 
-  @Autowired
-  private BranchRepository branchRepository;
-
-  @Autowired
-  private ContactRepository contactRepository;
-
-  private EntityObjectsBuilder builder = new EntityObjectsBuilder();
-
-  private Contact contact;
-
-  private static long id;
-
-  @Before
-  public void setup() {
-    Customer customer = builder.getCustomerObject();
-    customer = customerRepository.saveAndFlush(customer);
-    id = customer.getId();
-    log.debug("Persisted with id:{} in DB the test Customer object: {}", id, customer);
-
-  }
-
   @Test
   public void testPrePersistAddsMandatoryFields() {
-
-    Customer chanchu = new Customer();
-    chanchu.setName("Chanchu & Sons Pvt. Ltd.");
-    chanchu.setPan(RandomStringUtils.randomAlphanumeric(10));
-    Customer badaChanchu = customerRepository.saveAndFlush(chanchu);
-    assertThat(badaChanchu.getTds().doubleValue()).isEqualTo(0.10);
-    assertThat(badaChanchu.getInvoicePrefix()).isEqualTo("INV");
-    assertThat(badaChanchu.getCurrecny()).isEqualTo("INR");
-  }
-
-  @Test
-  public void testAddingBranchesToCustomer() {
-    Customer customer = customerRepository.findById(id).get();
-    Branch branch = builder.getBranchObject();
-    branch.setOwner(customer);
-    branchRepository.saveAndFlush(branch);
-    Page<Branch> branches = branchRepository.findAllByOwner_Id(customer.getId(), PageRequest.of(0, 10));
-    assertThat(branches.getContent().size()).isEqualTo(1);
-  }
-
-  @Test
-  public void testAddingContactToCustomer() {
-    contact = contactRepository.saveAndFlush(builder.getContactObject());
-    Optional<Customer> customer = customerRepository.findById(id);
-    customer.ifPresent(it -> {
-
-      it.setContact(contact);
-      customerRepository.saveAndFlush(it);
-    });
-    assertThat(customerRepository.findById(id).get().getContact()).isEqualTo(contact);
+    log.info("This test is only for documenting default value configration done via @DynamicInsert and @ColumnDefault");
+    Customer bhau = new Customer();
+    bhau.setName("Bhau & Sons Pvt. Ltd.");
+    bhau.setDomain(RandomStringUtils.randomAlphanumeric(8));
+    bhau = customerRepository.saveAndFlush(bhau);
+    Customer badaBhau = customerRepository.findById(bhau.getId()).get();
+    assertThat(badaBhau.getTds().doubleValue()).isEqualTo(0.10);
+    assertThat(badaBhau.getInvoicePrefix()).isEqualTo("INV");
+    assertThat(badaBhau.getCurrency()).isEqualTo("INR");
   }
 
 }
