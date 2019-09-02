5a9fc5c6b15997f4846aff218d52b73ca6273021
Anadi Misra <anadi.misra@agilityroots.com>
2019-09-02 10:17:38 +0530

    added all args constructor
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- src/main/java/com/agilityroots/invoicely/controller/BranchController.java
+++ src/main/java/com/agilityroots/invoicely/controller/BranchController.java
@@ -1,14 +1,15 @@
-/**
- * 3 Dec 2018 BranchController.java
- * data-api
- * Copyright 2018 Agility Roots Private Limited. All Rights Reserved
- */
+/*
+  3 Dec 2018 BranchController.java
+  data-api
+  Copyright 2018 Agility Roots Private Limited. All Rights Reserved
+*/
 package com.agilityroots.invoicely.controller;
 
 import com.agilityroots.invoicely.entity.Branch;
 import com.agilityroots.invoicely.entity.Contact;
 import com.agilityroots.invoicely.resource.assembler.BranchResourceAssembler;
 import com.agilityroots.invoicely.service.BranchService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
@@ -34,23 +35,21 @@ import java.util.Optional;
 @Slf4j
 @RestController
 @ExposesResourceFor(Branch.class)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BranchController {
 
-  @Autowired
-  private BranchService branchService;
+  private final BranchService branchService;
 
-  @Autowired
-  private BranchResourceAssembler assembler;
+  private final BranchResourceAssembler assembler;
 
   @GetMapping("/branches/{id}")
-  public DeferredResult<ResponseEntity<Resource<Branch>>> getBranch(@PathVariable Long id, HttpServletRequest request) {
+  public DeferredResult<ResponseEntity<Resource<Branch>>> getBranch(@PathVariable Long id) {
 
     DeferredResult<ResponseEntity<Resource<Branch>>> response = new DeferredResult<>();
     response.onTimeout(
         () -> response.setErrorResult(ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Request timed out.")));
-    response.onError((Throwable t) -> {
-      response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured."));
-    });
+    response.onError((Throwable t) ->
+        response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured.")));
     ListenableFuture<Optional<Branch>> future = branchService.getBranch(id);
 
     future.addCallback(new ListenableFutureCallback<Optional<Branch>>() {
@@ -64,7 +63,7 @@ public class BranchController {
 
       @Override
       public void onFailure(Throwable ex) {
-        log.error("Failed loading branch details for id {} due to error {}", id, ex.getMessage(), ex.getStackTrace());
+        log.error("Failed loading branch details due to error {}", ex.getMessage(), ex.getStackTrace());
         response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body("Cannot get branch details due to server error."));
       }
@@ -76,13 +75,12 @@ public class BranchController {
 
   @PutMapping("/branches/{id}/contact")
   public DeferredResult<ResponseEntity<Object>> addContact(@PathVariable("id") Long id, HttpServletRequest request,
-                                                           @RequestBody(required = true) @Valid Contact contact) {
+                                                           @RequestBody @Valid Contact contact) {
     DeferredResult<ResponseEntity<Object>> response = new DeferredResult<>();
     response.onTimeout(
         () -> response.setErrorResult(ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Request timed out.")));
-    response.onError((Throwable t) -> {
-      response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured."));
-    });
+    response.onError(
+        (Throwable t) -> response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.")));
 
     ListenableFuture<Optional<URI>> future = branchService.addContact(id, contact, getCurrentLocation(request));
 
@@ -110,9 +108,8 @@ public class BranchController {
     DeferredResult<ResponseEntity<Resource<Contact>>> response = new DeferredResult<>();
     response.onTimeout(
         () -> response.setErrorResult(ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Request timed out.")));
-    response.onError((Throwable t) -> {
-      response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured."));
-    });
+    response.onError(
+        (Throwable t) -> response.setErrorResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured.")));
 
     ListenableFuture<Optional<Contact>> future = branchService.getBranchContact(id);
 
@@ -120,7 +117,7 @@ public class BranchController {
       @Override
       public void onSuccess(Optional<Contact> result) {
         response.setResult(
-            result.map(it -> new Resource<Contact>(it, new Link(getCurrentLocation(request).toString(), "contact")))
+            result.map(it -> new Resource<>(it, new Link(getCurrentLocation(request).toString(), "contact")))
                 .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()));
       }
 
