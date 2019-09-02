60d09bbfa98d8d7fd4070ebb63d22643fc36526b
Anadi Misra <anadi.misra@agilityroots.com>
2019-09-02 09:45:04 +0530

    removed invoice methods
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- src/main/java/com/agilityroots/invoicely/repository/CustomerRepository.java
+++ src/main/java/com/agilityroots/invoicely/repository/CustomerRepository.java
@@ -5,12 +5,14 @@
  */
 package com.agilityroots.invoicely.repository;
 
+import com.agilityroots.invoicely.entity.Branch;
 import com.agilityroots.invoicely.entity.Customer;
 import com.agilityroots.invoicely.entity.Invoice;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
@@ -37,28 +39,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
   @CachePut("customers")
   <S extends Customer> S saveAndFlush(S entity);
 
-
-  Page<Invoice> findByIdAndInvoice_PaymentDateIsNotNull(Long id, Pageable pageable);
-
-  /*
-   * Due invoices by Customer
-   * @param id
-   * @param today
-   * @param pageable
-   * @return
-   */
-  @Cacheable("invoices")
   @Lock(LockModeType.OPTIMISTIC)
-  Page<Invoice> findByIdAndInvoice_DueDateAfterAndInvoice_PaymentsIsNull(Long id, Date today, Pageable pageable);
-
-  /*
-   * Overdue Invoiced by Customer
-   * @param id
-   * @param today
-   * @param pageable
-   * @return
-   */
-  @Cacheable("invoices")
-  @Lock(LockModeType.OPTIMISTIC)
-  Page<Invoice> findByIdAndInvoice_DueDateBeforeAndInvoice_PaymentsIsNull(Long id, Date today, Pageable pageable);
+  @Cacheable("customers")
+  Optional<Customer> findOneByOrganisationId(String customerId);
 }
